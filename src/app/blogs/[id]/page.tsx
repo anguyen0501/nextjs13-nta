8578396573f026b33106f9e0b8b3f4dd91c903c7
anguyen0501 <20h1120185@ut.edu.vn>
2023-12-22 22:14:17 +0700

    call data, params id, view detail blogs

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,29 +1,48 @@
-'use client'
-
+"use client";
 import { useRouter } from "next/navigation";
-import { Button, Card } from 'react-bootstrap';
-
-const ViewDetailBlog = () => {
-    const router = useRouter()
-    const handleButton = () => {
-        router.push("/blogs")
+import { Button, Card } from "react-bootstrap";
+import useSWR, {Fetcher} from "swr";
+const ViewDetailBlog = ({params}: {params : {id : string}}) => {
+  const router = useRouter();
+  const handleButton = () => {
+    router.push("/blogs");
+  };
+  const fetcher: Fetcher<IBlog, string> = (url: string) => fetch(url).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    `http://localhost:8000/blogs/${params.id}`,
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
     }
+  );
 
-    return ( 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch("http://localhost:8000/blogs");
+  //     const data = await res.json();
+  //     console.log("check data", data);
+  //   }
+  //   fetchData();
+  // }, []);
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+  return (
     <>
-        <Card className="my-3 text-center">
-            <Card.Header>Featured</Card.Header>
-            <Card.Body>
-                <Card.Title>Blog Title</Card.Title>
-                <Card.Text>
-                Blog Content
-                </Card.Text>
-                <Button variant="success" onClick={()=> handleButton()}>Back Blogs</Button>
-            </Card.Body>
-            <Card.Footer className="text-muted">author</Card.Footer>
-        </Card>
-    </> 
-    );
-}
- 
+      <Card className="my-3 text-center">
+        <Card.Header>Blog ID: {data?.id}</Card.Header>
+        <Card.Body>
+          <Card.Title>Blog Title: {data?.title}</Card.Title>
+          <Card.Text>{data?.content}</Card.Text>
+          <Button variant="success" onClick={() => handleButton()}>
+            Back Blogs
+          </Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">Author: {data?.author}</Card.Footer>
+      </Card>
+    </>
+  );
+};
+
 export default ViewDetailBlog;
