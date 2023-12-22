948df756a8d67d779ce6b18160c8f4e28a9d33fd
anguyen0501 <20h1120185@ut.edu.vn>
2023-12-22 21:52:48 +0700

    api view detail

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRouter } from "next/navigation";
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
 const ViewDetailBlog = () => {
     const router = useRouter()
@@ -11,9 +11,19 @@ const ViewDetailBlog = () => {
 
     return ( 
     <>
-        <h1>View Detail Blog</h1>
-        <Button variant="success" onClick={()=> handleButton()}>Back Blogs</Button>
-    </> );
+        <Card className="my-3 text-center">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+                <Card.Title>Blog Title</Card.Title>
+                <Card.Text>
+                Blog Content
+                </Card.Text>
+                <Button variant="success" onClick={()=> handleButton()}>Back Blogs</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">author</Card.Footer>
+        </Card>
+    </> 
+    );
 }
  
 export default ViewDetailBlog;
--- src/components/app.table.tsx
+++ src/components/app.table.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
-import Table from 'react-bootstrap/Table';
-import { Button } from 'react-bootstrap';
-import CreateModal from './create.modal';
-import UpdateModal from './update.modal';
-import { useState } from 'react';
-import Link from 'next/link';
+import Table from "react-bootstrap/Table";
+import { Button } from "react-bootstrap";
+import CreateModal from "./create.modal";
+import UpdateModal from "./update.modal";
+import { useState } from "react";
+import Link from "next/link";
 interface IProps {
   blogs: IBlog[];
 }
@@ -19,47 +19,66 @@ function AppTable(props: IProps) {
   const [showModalUpdate, setShowModalUpdate] = useState<boolean>(false);
   return (
     <>
-    <div className='mb-3' style={{display: "flex", justifyContent: "space-between"}}>
-      <h3>Table Blogs</h3>
-      <Button variant="primary" onClick={() => setShowModalCreate(true)}>Add New</Button>
-    </div>
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>No</th>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {blogs.map((item) => {
-          return (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.title}</td>
-              <td>{item.author}</td>
-              <td>
-                  <Link href={`/blogs/${item.id}`} className='mx-3 btn btn-primary'>Detail</Link>
-                <Button variant="primary" onClick={() => {setBlog(item); setShowModalUpdate(true);}}>Edit</Button>
-                <Button variant="danger" className='mx-3'>Delete</Button>
-              </td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </Table>
-    <CreateModal 
-      showModalCreate={showModalCreate}
-      setShowModalCreate={setShowModalCreate}
-    />
-    <UpdateModal
-      showModalUpdate={showModalUpdate}
-      setShowModalUpdate={setShowModalUpdate}
-      blog={blog}
-      setBlog={setBlog}
-    />
-
+      <div
+        className="mb-3"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <h3>Table Blogs</h3>
+        <Button variant="primary" onClick={() => setShowModalCreate(true)}>
+          Add New
+        </Button>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {blogs.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.author}</td>
+                <td>
+                  <Link
+                    href={`/blogs/${item.id}`}
+                    className="mx-3 btn btn-primary"
+                  >
+                    Detail
+                  </Link>
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      setBlog(item);
+                      setShowModalUpdate(true);
+                    }}
+                  >
+                    Edit
+                  </Button>
+                  <Button variant="danger" className="mx-3">
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <CreateModal
+        showModalCreate={showModalCreate}
+        setShowModalCreate={setShowModalCreate}
+      />
+      <UpdateModal
+        showModalUpdate={showModalUpdate}
+        setShowModalUpdate={setShowModalUpdate}
+        blog={blog}
+        setBlog={setBlog}
+      />
     </>
   );
 }
